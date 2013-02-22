228e7a3f0881e14eac514a7872d320c8235d2bb8
StevenKwok <guozhaolong@gmail.com>
2013-02-22 16:49:01 +0800

    add Post Controller

---
+++ app/controllers/Post.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import models.TPost;
+import models.TUser;
+import org.codehaus.jackson.node.ObjectNode;
+import play.cache.Cache;
+import play.libs.Json;
+import play.mvc.BodyParser;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import java.util.List;
+
+public class Post extends Controller {
+
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result list(){
+        ObjectNode result = Json.newObject();
+        TUser user = TUser.find.byId(Long.valueOf(session("userid")));
+        List<TPost> posts = TUser.findPosts(user);
+        Cache.set("", "");
+        return ok(result);
+    }
+
+}
