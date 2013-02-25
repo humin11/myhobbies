ef750f154a6f2c1271dba334bfd48ef9573d1b41
Hu Min <minispring1013@gmail.com>
2013-02-26 00:32:17 +0800

    Modify court index page.

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,21 +3,37 @@
 
 # --- !Ups
 
-create table comment (
+create table aspects (
+  id                        bigint auto_increment not null,
+  author_id                 bigint,
+  create_at                 datetime,
+  name                      varchar(255),
+  constraint pk_aspects primary key (id))
+;
+
+create table aspect_members (
+  aspect_id                 bigint,
+  contact_id                bigint)
+;
+
+create table comments (
   id                        bigint auto_increment not null,
   content                   varchar(255),
   author_id                 bigint,
   create_at                 datetime,
   modify_at                 datetime,
   status                    varchar(255),
+  post_id                   bigint,
   type                      varchar(255),
-  constraint pk_comment primary key (id))
+  constraint pk_comments primary key (id))
 ;
 
-create table contact (
+create table contacts (
   id                        bigint auto_increment not null,
+  owner_id                  bigint,
+  member_id                 bigint,
   create_at                 datetime,
-  constraint pk_contact primary key (id))
+  constraint pk_contacts primary key (id))
 ;
 
 create table court (
@@ -36,28 +52,25 @@ create table court (
   constraint pk_court primary key (id))
 ;
 
-create table groups (
-  id                        bigint auto_increment not null,
-  author_id                 bigint,
-  create_at                 datetime,
-  name                      varchar(255),
-  constraint pk_groups primary key (id))
-;
-
-create table groups_member (
-  group_id                  bigint,
-  contact_id                bigint)
-;
-
 create table likes (
   id                        bigint auto_increment not null,
+  post_id                   bigint,
+  comment_id                bigint,
   type                      varchar(255),
   author_id                 bigint,
   create_at                 datetime,
   constraint pk_likes primary key (id))
 ;
 
-create table person (
+create table mentions (
+  id                        bigint auto_increment not null,
+  user_id                   bigint,
+  post_id                   bigint,
+  comment_id                bigint,
+  constraint pk_mentions primary key (id))
+;
+
+create table people (
   id                        bigint auto_increment not null,
   nickname                  varchar(255),
   summary                   varchar(255),
@@ -67,10 +80,10 @@ create table person (
   height                    float,
   weight                    float,
   user_id                   bigint,
-  constraint pk_person primary key (id))
+  constraint pk_people primary key (id))
 ;
 
-create table photo (
+create table photos (
   id                        bigint auto_increment not null,
   post_id                   bigint,
   author_id                 bigint,
@@ -81,10 +94,10 @@ create table photo (
   name                      varchar(255),
   photo_path                varchar(255),
   photo_name                varchar(255),
-  constraint pk_photo primary key (id))
+  constraint pk_photos primary key (id))
 ;
 
-create table post (
+create table posts (
   id                        bigint auto_increment not null,
   content                   varchar(255),
   author_id                 bigint,
@@ -95,19 +108,19 @@ create table post (
   shareable                 tinyint(1) default 0,
   commentable               tinyint(1) default 0,
   likeable                  tinyint(1) default 0,
-  constraint pk_post primary key (id))
+  constraint pk_posts primary key (id))
 ;
 
-create table post_share (
+create table post_shares (
   post_id                   bigint,
-  share_person_id           bigint,
-  share_group_id            bigint,
-  share_type                varchar(255),
+  person_id                 bigint,
+  aspect_id                 bigint,
+  type                      varchar(255),
   author_id                 bigint,
   create_at                 datetime)
 ;
 
-create table user (
+create table users (
   id                        bigint auto_increment not null,
   email                     varchar(255),
   username                  varchar(255),
@@ -120,39 +133,55 @@ create table user (
   locked_at                 datetime,
   unlocked_at               datetime,
   person_id                 bigint,
-  constraint pk_user primary key (id))
+  constraint pk_users primary key (id))
 ;
 
-alter table comment add constraint fk_comment_author_1 foreign key (author_id) references user (id) on delete restrict on update restrict;
-create index ix_comment_author_1 on comment (author_id);
-alter table groups add constraint fk_groups_author_2 foreign key (author_id) references user (id) on delete restrict on update restrict;
-create index ix_groups_author_2 on groups (author_id);
-alter table groups_member add constraint fk_groups_member_group_3 foreign key (group_id) references groups (id) on delete restrict on update restrict;
-create index ix_groups_member_group_3 on groups_member (group_id);
-alter table groups_member add constraint fk_groups_member_contact_4 foreign key (contact_id) references contact (id) on delete restrict on update restrict;
-create index ix_groups_member_contact_4 on groups_member (contact_id);
-alter table likes add constraint fk_likes_author_5 foreign key (author_id) references user (id) on delete restrict on update restrict;
-create index ix_likes_author_5 on likes (author_id);
-alter table person add constraint fk_person_user_6 foreign key (user_id) references user (id) on delete restrict on update restrict;
-create index ix_person_user_6 on person (user_id);
-alter table photo add constraint fk_photo_post_7 foreign key (post_id) references post (id) on delete restrict on update restrict;
-create index ix_photo_post_7 on photo (post_id);
-alter table photo add constraint fk_photo_author_8 foreign key (author_id) references user (id) on delete restrict on update restrict;
-create index ix_photo_author_8 on photo (author_id);
-alter table post add constraint fk_post_author_9 foreign key (author_id) references user (id) on delete restrict on update restrict;
-create index ix_post_author_9 on post (author_id);
-alter table post add constraint fk_post_parent_10 foreign key (parent_id) references post (id) on delete restrict on update restrict;
-create index ix_post_parent_10 on post (parent_id);
-alter table post_share add constraint fk_post_share_post_11 foreign key (post_id) references post (id) on delete restrict on update restrict;
-create index ix_post_share_post_11 on post_share (post_id);
-alter table post_share add constraint fk_post_share_share_person_12 foreign key (share_person_id) references user (id) on delete restrict on update restrict;
-create index ix_post_share_share_person_12 on post_share (share_person_id);
-alter table post_share add constraint fk_post_share_share_group_13 foreign key (share_group_id) references groups (id) on delete restrict on update restrict;
-create index ix_post_share_share_group_13 on post_share (share_group_id);
-alter table post_share add constraint fk_post_share_author_14 foreign key (author_id) references user (id) on delete restrict on update restrict;
-create index ix_post_share_author_14 on post_share (author_id);
-alter table user add constraint fk_user_person_15 foreign key (person_id) references person (id) on delete restrict on update restrict;
-create index ix_user_person_15 on user (person_id);
+alter table aspects add constraint fk_aspects_author_1 foreign key (author_id) references users (id) on delete restrict on update restrict;
+create index ix_aspects_author_1 on aspects (author_id);
+alter table aspect_members add constraint fk_aspect_members_aspect_2 foreign key (aspect_id) references aspects (id) on delete restrict on update restrict;
+create index ix_aspect_members_aspect_2 on aspect_members (aspect_id);
+alter table aspect_members add constraint fk_aspect_members_contact_3 foreign key (contact_id) references contacts (id) on delete restrict on update restrict;
+create index ix_aspect_members_contact_3 on aspect_members (contact_id);
+alter table comments add constraint fk_comments_author_4 foreign key (author_id) references users (id) on delete restrict on update restrict;
+create index ix_comments_author_4 on comments (author_id);
+alter table comments add constraint fk_comments_post_5 foreign key (post_id) references posts (id) on delete restrict on update restrict;
+create index ix_comments_post_5 on comments (post_id);
+alter table contacts add constraint fk_contacts_owner_6 foreign key (owner_id) references users (id) on delete restrict on update restrict;
+create index ix_contacts_owner_6 on contacts (owner_id);
+alter table contacts add constraint fk_contacts_member_7 foreign key (member_id) references users (id) on delete restrict on update restrict;
+create index ix_contacts_member_7 on contacts (member_id);
+alter table likes add constraint fk_likes_post_8 foreign key (post_id) references posts (id) on delete restrict on update restrict;
+create index ix_likes_post_8 on likes (post_id);
+alter table likes add constraint fk_likes_comment_9 foreign key (comment_id) references comments (id) on delete restrict on update restrict;
+create index ix_likes_comment_9 on likes (comment_id);
+alter table likes add constraint fk_likes_author_10 foreign key (author_id) references users (id) on delete restrict on update restrict;
+create index ix_likes_author_10 on likes (author_id);
+alter table mentions add constraint fk_mentions_user_11 foreign key (user_id) references users (id) on delete restrict on update restrict;
+create index ix_mentions_user_11 on mentions (user_id);
+alter table mentions add constraint fk_mentions_post_12 foreign key (post_id) references posts (id) on delete restrict on update restrict;
+create index ix_mentions_post_12 on mentions (post_id);
+alter table mentions add constraint fk_mentions_comment_13 foreign key (comment_id) references comments (id) on delete restrict on update restrict;
+create index ix_mentions_comment_13 on mentions (comment_id);
+alter table people add constraint fk_people_user_14 foreign key (user_id) references users (id) on delete restrict on update restrict;
+create index ix_people_user_14 on people (user_id);
+alter table photos add constraint fk_photos_post_15 foreign key (post_id) references posts (id) on delete restrict on update restrict;
+create index ix_photos_post_15 on photos (post_id);
+alter table photos add constraint fk_photos_author_16 foreign key (author_id) references users (id) on delete restrict on update restrict;
+create index ix_photos_author_16 on photos (author_id);
+alter table posts add constraint fk_posts_author_17 foreign key (author_id) references users (id) on delete restrict on update restrict;
+create index ix_posts_author_17 on posts (author_id);
+alter table posts add constraint fk_posts_parent_18 foreign key (parent_id) references posts (id) on delete restrict on update restrict;
+create index ix_posts_parent_18 on posts (parent_id);
+alter table post_shares add constraint fk_post_shares_post_19 foreign key (post_id) references posts (id) on delete restrict on update restrict;
+create index ix_post_shares_post_19 on post_shares (post_id);
+alter table post_shares add constraint fk_post_shares_person_20 foreign key (person_id) references users (id) on delete restrict on update restrict;
+create index ix_post_shares_person_20 on post_shares (person_id);
+alter table post_shares add constraint fk_post_shares_aspect_21 foreign key (aspect_id) references aspects (id) on delete restrict on update restrict;
+create index ix_post_shares_aspect_21 on post_shares (aspect_id);
+alter table post_shares add constraint fk_post_shares_author_22 foreign key (author_id) references users (id) on delete restrict on update restrict;
+create index ix_post_shares_author_22 on post_shares (author_id);
+alter table users add constraint fk_users_person_23 foreign key (person_id) references people (id) on delete restrict on update restrict;
+create index ix_users_person_23 on users (person_id);
 
 
 
@@ -160,27 +189,29 @@ create index ix_user_person_15 on user (person_id);
 
 SET FOREIGN_KEY_CHECKS=0;
 
-drop table comment;
+drop table aspects;
 
-drop table contact;
+drop table aspect_members;
+
+drop table comments;
+
+drop table contacts;
 
 drop table court;
 
-drop table groups;
-
-drop table groups_member;
-
 drop table likes;
 
-drop table person;
+drop table mentions;
 
-drop table photo;
+drop table people;
 
-drop table post;
+drop table photos;
 
-drop table post_share;
+drop table posts;
 
-drop table user;
+drop table post_shares;
+
+drop table users;
 
 SET FOREIGN_KEY_CHECKS=1;
 
